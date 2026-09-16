5835b536022f6005f0e87d24d806e01bd149e8f6
ARON JOVIN SF <unknownuser111786@gmail.com>
2026-09-16 12:13:44 +0530

    ARITHMATIC OPERATION
    PYTHON PROGRAM FOR ARITHMATIC OPERATION

---
+++ Day-01-Arithmatic_Operation/ARITHMETIC OPERATIONS.py
@@ -0,0 +1,25 @@
+# Python program for Arithmetic Operations
+
+a = 20
+b = 6
+
+# Addition
+print("Addition:", a + b)
+
+# Subtraction
+print("Subtraction:", a - b)
+
+# Multiplication
+print("Multiplication:", a * b)
+
+# Division
+print("Division:", a / b)
+
+# Modulus (remainder)
+print("Modulus:", a % b)
+
+# Exponentiation (power)
+print("Exponentiation:", a ** b)
+
+# Floor Division
+print("Floor Division:", a // b)
